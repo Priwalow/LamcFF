c8c417fd1df3d9b73c62bd1ccb60c4ce7dd65dc8
Sergey Privalov <privalov@serval.lebedev.ru>
2019-10-23 14:56:08 +0300

    Square of nu_mu recoil mass

--- analysis/hmerge/Nu_mu.C
+++ analysis/hmerge/Nu_mu.C
@@ -3,22 +3,22 @@
     TChain* ch1dat = new TChain("h1"); //without pi0
     ch1dat -> Add("*.root");
     
-    double lend=-0.1, rend=0.1;
-    int Nbins=20;
+    double lend=-1, rend=1;
+    int Nbins=100;
     TCanvas *c1 = new TCanvas("c1","Muon neutrino invariant mass",740,600);
     TH1D* hdat = new TH1D("hdat","Squared recoil mass of #nu_{#mu}",Nbins,lend,rend);
     double hwidth = rend-lend, binw = hwidth/Nbins;
     
     double Ntot, Nsig, dNsig, Nbkg, dNbkg;
-    ch1dat -> Draw("rm*fabs(rm)>>hdat","lcch == 4 && ml > 1.105 && ml < 1.125 && m*m-10.58*10.58+2*10.58*p-rm*rm > -0.2 && m*m-10.58*10.58+2*10.58*p-rm*rm < 0.2 && p<1","goff"); 
+    ch1dat -> Draw("rm*fabs(rm)>>hdat","lcch == 4 && m*m-10.58*10.58+2*10.58*p-rm*rm > -2 && m*m-10.58*10.58+2*10.58*p-rm*rm < 2  && p>0.3","goff"); 
     
     hdat -> Scale(1,"width");
     
-    TF1* fdat = new TF1("fdat","[0]*TMath::Gaus(x,[1],[2],true)+[3]",lend,rend);
+    TF1* fdat = new TF1("fdat","[0]*TMath::Gaus(x,[1],[2],true)+[3]*TMath::Gaus(x,[4],[5],true)+[6]*TMath::Gaus(x,[7],[8],true)",lend,rend);
     
 
-    fdat -> SetParameters(100,0,0.05,10);
-    fdat -> SetParLimits(1,-0.02,0.02);
+    fdat -> SetParameters(58,0.02,0.1,13,-0.005,1.59,6.1,0.2,0.5);
+    //fdat -> SetParLimits(1,-0.02,0.02);
 
     hdat -> Fit("fdat","L S M N Q","goff"); //L S M N Q
 
@@ -37,8 +37,8 @@
     Nsig = fdat->GetParameter(0);
     dNsig = fdat->GetParError(0);
     
-    Nbkg = fdat -> GetParameter(3)*hwidth;
-    dNbkg = fdat -> GetParError(3)*hwidth;
+    Nbkg = fdat -> GetParameter(3)+fdat -> GetParameter(6);
+    dNbkg = sqrt(fdat -> GetParError(3)*fdat -> GetParError(3)+fdat -> GetParError(6)*fdat -> GetParError(6));
     
     //cout << "Total number of events: " << Ntot << endl;
     cout << "N nu_mu: " << (int) Nsig <<" +- " << (int) dNsig << endl;
@@ -48,8 +48,9 @@
     int ndf = Nbins - fdat ->GetNumberFreeParameters(); // nbins - npar
     cout << "ChiSquare / ndf: " << chisq << " / " << ndf << " = " << chisq/ndf << endl;
 
-    TF1* fbkg = new TF1("fbkg","[0]",lend,rend);    
-    fbkg -> SetParameter(0,fdat->GetParameter(3));
+    TF1* fbkg = new TF1("fbkg","[0]*TMath::Gaus(x,[1],[2],true)+[3]*TMath::Gaus(x,[4],[5],true)",lend,rend);    
+    fbkg -> SetParameters(fdat->GetParameter(3),fdat->GetParameter(4),fdat->GetParameter(5),
+			fdat->GetParameter(6), fdat->GetParameter(7), fdat->GetParameter(8));
     fbkg -> SetLineStyle(2);
     fbkg -> SetLineColor(12);
     fbkg -> SetLineWidth(3);
