8f3165af50f1458aff0fc84f088a194581fbf47d
Sergey Privalov <priwalowsa@gmail.com>
2020-03-28 19:25:46 +0300

    Fixing

--- analysis/User_reco.cc
+++ analysis/User_reco.cc
@@ -405,9 +405,9 @@ namespace Belle {
                 
                 Hep3Vector norm_lambda, norm_W;
                 norm_lambda = (boostT(p_proton_from_lam, p_lamc).vect()).cross(boostT(p_pi_from_lam, p_lamc).vect());
-                norm_lambda = norm_lambda/(norm_lambda.mag());
+                norm_lambda = norm_lambda*(1./norm_lambda.mag());
                 norm_W = (boostT(p_nu, p_lamc).vect()).cross(boostT(p_l, p_lamc).vect()); 
-                norm_W = norm_W/(norm_W.mag());
+                norm_W = norm_W/(1./norm_W.mag());
                 
                 if ((lcch==3) || (lcch==4))
                 {
