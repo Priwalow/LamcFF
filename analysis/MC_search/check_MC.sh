596a8a7e307a8bcaf8aeeaad89e96b383ffb228b
Sergey Privalov <priwalowsa@gmail.com>
2020-11-15 23:20:20 +0300

    Debugging

--- analysis/MC_search/check_MC.sh
+++ analysis/MC_search/check_MC.sh
@@ -4,5 +4,5 @@ exp=$1
 run=$2
 run1=$3                                                                                                                                                                                               
 (
-check_process_url "http://bweb3/montecarlo.php?bl=caseB&ex=${exp}$&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dv=zfserv&dt=Any"
+check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv"
 )|wc -l 
