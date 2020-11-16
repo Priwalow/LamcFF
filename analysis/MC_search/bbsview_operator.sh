b0353def479724e6a80f8909cf7c77f469830e97
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 22:45:41 +0300

    Editing

--- analysis/MC_search/bbsview_operator.sh
+++ analysis/MC_search/bbsview_operator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
-spawn ./bbsview
+spawn ./bbsview /group/belle/bdata_b/mcprod/dat/e000035/evtgen/mixed/07/all/0127/on_resonance/04/evtgen-mixed-07-all-e000035r000457-b20090127_0910.mdst
 expect "*\r"
 send -- "L\r"
 expect "*\r"
