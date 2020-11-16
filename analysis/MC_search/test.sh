e7dbccf8134a9162d4f47a5db330e0af9b3046b1
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 18:43:32 +0300

    Editing

--- analysis/MC_search/test.sh
+++ analysis/MC_search/test.sh
@@ -7,7 +7,8 @@ run1=$3
 check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv" > mc_files.txt
 
 while read i
-do 
-    bbsview $(echo "$i" | cut -d ' ' -f 2)
+do
     
+    eval fn=$(echo "$i" | cut -d ' ' -f 2)
+    bbsview fn 
 done < mc_files.txt
