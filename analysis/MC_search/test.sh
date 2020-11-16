20f52b1545924d8901cfcccc1fdaddedb74bcdf5
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 15:04:40 +0300

    Editing

--- analysis/MC_search/test.sh
+++ analysis/MC_search/test.sh
@@ -5,7 +5,7 @@ run=$2
 run1=$3                                                                                                                                                                                               
 
 
-while read p
+for i in $(check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv")
 do 
-    echo "$p"
-done < (check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv") 
+    echo "$i"
+done 
