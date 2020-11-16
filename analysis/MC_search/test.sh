ff2c0fff0f4d234a1265953320ef5a0d194c3b3d
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 16:14:01 +0300

    Editing

--- analysis/MC_search/test.sh
+++ analysis/MC_search/test.sh
@@ -8,5 +8,5 @@ check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${
 
 while read i
 do 
-    echo `expr match "$i" '\</*.mdst\>'`
+    echo `expr "$i" : '\(.......\)'`
 done < mc_files.txt
