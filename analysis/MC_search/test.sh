e27697174f8a027182a43594f4cf5ad793adda2e
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 15:20:04 +0300

    Editing

--- analysis/MC_search/test.sh
+++ analysis/MC_search/test.sh
@@ -4,7 +4,11 @@ exp=$1
 run=$2
 run1=$3                                                                                                                                                                                               
 
-while read line;
+
+filelist= check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv"
+
+
+for i filelist
 do 
-    echo line
-done < $(check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv")
+    echo $i
+done
