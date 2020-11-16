670bfceacbcf7d2ff53811ff07924b18c3dd7616
Sergey Privalov <priwalowsa@gmail.com>
2020-11-16 15:01:06 +0300

    Editing

--- analysis/MC_search/test.sh
+++ analysis/MC_search/test.sh
@@ -8,4 +8,4 @@ run1=$3
 while read p
 do 
     echo "$p"
-done < check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv" 
+done < (check_process_url "http://bweb3/montecarlo.php?ex=${exp}&rs=${run}${run1}0&re=${run}${run1}9&ty=Any&dt=Any&bl=caseB&dv=zfserv") 
