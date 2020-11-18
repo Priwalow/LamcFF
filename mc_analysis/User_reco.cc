5ba778211db23d79039ae77e1c96571eb8c4300f
Sergey Privalov <priwalowsa@gmail.com>
2020-11-18 23:23:58 +0300

    Editing

--- mc_analysis/User_reco.cc
+++ mc_analysis/User_reco.cc
@@ -96,21 +96,21 @@ namespace Belle {
             {
                 cout << "1st Daugher! ";
                 mc_lam = Particle(*evt);
-                cout << mc_lam.mass() << "  " << idHEP << endl;
+                cout << mc_lam.mass() << "  " << idHEP << " " << evt->mo(0) << endl;
             }
             
             if(id == daid2) //Lam = 3122, anti-Lam = -3122
             {
                 cout << "2nd Daugher! ";
                 mc_lam = Particle(*evt);
-                cout << mc_lam.mass() << "  " << idHEP << endl;
+                cout << mc_lam.mass() << "  " << idHEP << " " << evt->mo(0) << endl;
             }
             
             if(id == daid3) //Lam = 3122, anti-Lam = -3122
             {
                 cout << "3rd Daugher! ";
                 mc_lam = Particle(*evt);
-                cout << mc_lam.mass() << "  " << idHEP << endl;
+                cout << mc_lam.mass() << "  " << idHEP << " " << evt->mo(0) << endl;
             }
             
             id++;
